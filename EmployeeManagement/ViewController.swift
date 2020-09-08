b9f18367d833720502f7078950c486dd11a28008
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-09-08 19:40:55 +0900

    create project

---
+++ EmployeeManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmployeeManagement
+//
+//  Created by SangHyuk Lee on 2020/09/08.
+//  Copyright © 2020 SangHyuk Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
