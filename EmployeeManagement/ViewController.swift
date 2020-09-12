25ae38d2d3a7819f3b628bb67ccafb1621787522
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-09-13 01:22:38 +0900

    fix

--- EmployeeManagement/ViewController.swift
+++ EmployeeManagement/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let sample = UIView()
+        sample.backgroundColor = .clear
+        sample.layer.borderColor = UIColor.lightGray.cgColor
+        sample.layer.borderWidth = 1
+        sample.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        sample.center = view.center
+        sample.layer.cornerRadius = sample.frame.height / 2
+        self.view.addSubview(sample)
+        
+        UIView.animate(withDuration: 1.0) {
+            sample.alpha = 1
+            sample.frame.size.height = 100
+            sample.frame.size.width = self.view.frame.width + 10
+            sample.center = self.view.center
+            sample.layer.cornerRadius = sample.frame.width
+        }
+        
+//        UIView.animate(withDuration: 0.5, animations: {
+//           sample.alpha = 1
+//            sample.frame.size.height = 100
+//            sample.frame.size.width = 100
+//            sample.center = self.view.center
+//            sample.layer.cornerRadius = sample.frame.height / 2
+//        }) { finished in
+//            UIView.animate(withDuration: 0.5, animations: {
+//                sample.alpha = 0
+//                sample.frame.size.height = 0
+//                sample.frame.size.width = 0
+//                sample.center = self.view.center
+//                sample.layer.cornerRadius = sample.frame.height / 2
+//            })
+//        }
+        
+        
     }
 
 
