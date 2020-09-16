a3f7e33ad0ca63b7f7ed9a46e1b526572602d559
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-09-16 11:14:43 +0900

    create new viewcontroller

---
+++ EmployeeManagement/ViewController/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  EmployeeManagement
+//
+//  Created by SangHyuk Lee on 2020/09/24.
+//  Copyright © 2020 SangHyuk Lee. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
