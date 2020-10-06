3cef788dc2f1de996e8f3c7fe4ca1e990bb36a69
sanghyuk-e <sanghyuk.eee@gmail.com>
2020-10-06 19:19:01 +0900

    add save button, fix constraint

--- EmployeeManagement/ViewController/HomeViewController.swift
+++ EmployeeManagement/ViewController/HomeViewController.swift
@@ -17,6 +17,9 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // 버튼 세팅
+        setButtons()
+        
         sampleButtom.states = ["On", "Off"]
         sampleButtom.backgroundColors = [.green, .gray]
         
@@ -24,6 +27,19 @@ class HomeViewController: UIViewController {
             print(self.sampleButtom.currentStateIndex)
         }
 
+    }
+    
+    
+    //MARK: - Function
+    
+    /// #버튼 세팅
+    ///
+    /// - Authors: Jack
+    /// - Date: 2020-10-06
+    /// - Requires: 버튼을 세팅하는 함수
+    /// - Important: 파이어베이스에서 마지막 저장 상태를 불러와야 한다.
+    func setButtons() {
+        
     }
 
 }
